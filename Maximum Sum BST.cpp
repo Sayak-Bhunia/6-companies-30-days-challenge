00abca1eacdcec00bd15b490641b861fb0f52f1e
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-01-04 14:39:23 +0530

    Create Maximum Sum BST.cpp

---
+++ Maximum Sum BST.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class BST {
+    public:
+    int sum;
+    int mini;
+    int maxi;
+    BST(int sum = 0,int mini = INT_MAX,int maxi=INT_MIN) {
+        this->sum = sum;
+        this->mini = mini;
+        this->maxi = maxi;
+    }
+};
+
+int ans = 0;
+BST helper(TreeNode *root) {
+    if(!root) return BST(0,INT_MAX,INT_MIN);
+    auto lt = helper(root->left);
+    auto rt = helper(root->right);
+    if(root->val>lt.maxi && root->val<rt.mini) {
+        int sum = root->val+lt.sum+rt.sum;
+        ans = max(ans,sum);
+        return BST(sum,min(root->val,lt.mini),max(root->val,rt.maxi));
+    }
+    return BST(max(lt.sum,rt.sum),INT_MIN,INT_MAX);
+}
+
+class Solution {
+public:
+    int maxSumBST(TreeNode* root) {
+        ans = 0;
+        helper(root);
+        return ans;
+    }
+};

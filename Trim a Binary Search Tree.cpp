b5d98d957423bc0fe8372b09b65357bc2ac4e753
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-01-18 09:02:40 +0530

    Create Trim a Binary Search Tree.cpp

---
+++ Trim a Binary Search Tree.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int l, int h) {
+        if(root == NULL) return NULL;
+        else if(root->val<l) return trimBST(root->right,l,h);
+        else if(root->val>h) return trimBST(root->left,l,h);
+        root->left = trimBST(root->left,l,h);
+        root->right = trimBST(root->right,l,h);
+        return root;
+    }
+};

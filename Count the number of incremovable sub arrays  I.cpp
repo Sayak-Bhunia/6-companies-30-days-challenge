94ff3356c4990ba7be82a874e0acaca0a5709791
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-01-23 23:33:43 +0530

    Update Count the number of incremovable sub arrays  I.cpp

--- Count the number of incremovable sub arrays  I.cpp
+++ Count the number of incremovable sub arrays  I.cpp
@@ -1,15 +1,24 @@
-int incremovableSubarrayCount(vector<int>& nums) {
-    int ans = 0;
-    for(int i = 0; i < nums.size(); ++i){
-        for(int j = i; j < nums.size(); ++j){
-            int last = -1, flag = 1;
-            for(int k = 0; k < nums.size(); ++k){
-                if(k >= i && k <= j) continue;
-                if(last >= nums[k]){ flag = 0; break; }
-                last = nums[k];
-            }
-            if(flag) ans++;
+class Solution {
+public:
+    int incremovableSubarrayCount(vector<int>& a) {
+        int n = a.size();
+        int i=0;
+        for(;i<n-1;i++){
+            if(a[i+1]<=a[i]) break;
         }
+        int j=n-1;
+        for(;j>0;j--){
+            if(a[j-1]>=a[j]) break;
+        }
+        int c=0;
+        while(i>=0){
+            int u = upper_bound(a.begin()+j, a.end(), a[i])-a.begin();
+            c += n-u+1;
+            i--;
+        }
+        if(c<(n*(n+1))/2){
+            c += n-j+1;
+        }
+        return c;
     }
-    return ans;
-}
+};
